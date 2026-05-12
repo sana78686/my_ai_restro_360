119a56ce249aa779c8858b06ffa3336e441c53da
sankhan55 <sana.fatima@netopz.com>
2026-05-12 06:31:44 +0500

    mail  works fine now

--- config/mail.php
+++ config/mail.php
@@ -46,6 +46,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+            // 'verify_peer' => false, // Disable SSL certificate verification for AWS SES
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
